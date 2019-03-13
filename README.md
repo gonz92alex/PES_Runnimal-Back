214c741ff20e3124e47c3425f5458f6207663438
Alejandro <gonzalex92@gmail.com>
2019-03-13 20:09:09 +0100

    Add modules to server

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Runnimal
 
+install Node.js and Express
+https://www.vultr.com/docs/installing-node-js-and-express
+
 Always do npm install --save [MODULE]
--- app.js
+++ app.js
@@ -1,3 +1,80 @@
+console.log('Reading configuration files...');
+const config = require('./config/config.js');
+// Read the configuration variables
+var PORT  = config.get('server.port');
+var DB = config.get('db.host');
+// Print some of them just for information
+console.log('Read PORT from config: ' + PORT);
+console.log('Read Database host DB from config: ' + DB);
+/* Models */
+//let user = require('./app/models/user');
+
+/* express routing dependency. Add all your modules here */
+var express = require('express'),
+    app = express(),
+    port = PORT,
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    engine = require('ejs-blocks')
+    //Contribution = require('./app/models/contribution'),
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+const path = require('path');
+
+/* Deployment settings */
+mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify', false);
+
+/* Connect mongoose object mapper to the database (mongoDB must be running) */
+mongoose.connect(DB,{ 
+    useNewUrlParser: true 
+});
+
+// CORS
+var cors = require('cors');
+app.use(cors());
+
+// Register some dependencies needed for the API
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// API implementation routes
+var apiRoutes = require('./routes/apiRoutes');
+/*
+// Session Routing
+var passport = require('passport');
+require('./config/passport')(passport);
+
+app.use(passport.initialize());
+app.use(passport.session());*/
+
+// Register the routes
+//apiRoutes(app);
+app.use('/', apiRoutes);
+
+
+// Register static resources, views folder and view engine
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Start the application
+app.listen(PORT);
+
+// All good!
+console.log('Runnimal started on port: ' + PORT);
+// And ends sometime
+module.exports = app
+
+
+
+
+/*
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -38,4 +115,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = app;*/
