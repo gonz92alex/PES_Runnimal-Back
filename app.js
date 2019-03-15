32c3ed99e3b34f48148af7165d45bdb2e058b345
Alejandro <gonzalex92@gmail.com>
2019-03-15 17:56:45 +0100

    Añadida conexion con el servidor de MongoDb

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//const MongoClient = require('mongodb').MongoClient;
+//const Server = require('mongodb').Server;
+
+var runnimalDb;
+
+/*
+MongoClient.connect('mongodb://nidorana.fib.upc.edu/runnimal' { useNewUrlParser: true }, (err, database) => {
+  if(err) return console.log(err);
+  else runnimalDb = database;
+});
+*/
+var mongoose = require('mongoose');
+//mongodb://'+DATABASEUSERNAME+':'+DATABASEPASSWORD+'@'+DATABASEHOST+':'DATABASEPORT+'/'+DATABASENAME
+mongoose.connect('mongodb://runnimal:Runnimal06@nidorana.fib.upc.edu:27017/runnimal',  
+                  { useNewUrlParser: true }).catch(function (reason) {
+  console.log('Unable to connect to the mongodb instance. Error: ', reason);
+});
 
 var  indexRoutes = require('./routes/apiRoutes');
 
