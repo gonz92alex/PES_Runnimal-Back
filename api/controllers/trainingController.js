0e8e9a132740bb1f024796a04ec329d2a11e9a1e
Sergi Ventura <venturasolsona.sergi@gmail.com>
2019-03-22 18:16:59 +0100

    visualiza y añade entramientos a través de un formulario

---
+++ api/controllers/trainingController.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var mongoose = require('mongoose').set('debug',true);
+var Training = require('../models/Training');
+
+
+exports.list = function(req,res) {
+    Training.find(function(err, trainings){
+    	if (err)
+            res.send(err);
+        else
+            res.json(trainings);
+    });
+};
+
+exports.save = function(name, description){
+	var training = new Training({name: name, description: description});
+	
+	training.save(function(){
+		res.render("trainingSaved");
+	});
+};
--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -1,9 +1,24 @@
 'use strict';
 module.exports = function(app) {
-  var users = require('../controllers/userController');
+
+	var users = require('../controllers/userController');
+	var training = require('../controllers/trainingController');
+	app.set('view engine', 'jade');
 
 
-  app.get('/api/users',users.list);
+	//USERS//
+	app.get('/api/users',users.list);
 
+	//TRAININGS//
+	app.get('/api/trainings', training.list);
+	app.get('/api/trainings/new', function(req, res){
+		res.render("newTrainingForm");
+	});  
+	app.post('/api/trainings/save', function(req, res){
+		var name = req.body.name;
+		var description = req.body.description;
+		
+		training.save(name, description);
+	});
 }
 
