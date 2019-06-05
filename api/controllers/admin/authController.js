ba681f802b2f8643811fda37f3d77cbaa48f70cc
venturasolsonasergi <venturasolsona.sergi@gmail.com>
2019-06-06 01:21:28 +0200

    millora

--- api/controllers/admin/authController.js
+++ api/controllers/admin/authController.js
@@ -12,16 +12,13 @@ exports.login = function(req, res){
 	/*comprovar si el usuari y la contraseña son correctes*/
 	Users.getOne(req.body.email).then(function(user){
 		if(user) {
+			//Falta comprovar la contrassenya
 			/*bcrypt.compare(password, usr.password).then(function(res){
 
 			})*/
 			if(user.role == "admin"){
 				req.session.user = user;
-				console.log("hem arribat");
-				console.log(req.session.user);
 				return res.redirect('/admin/users/');
-				//return res.send(req.session.user);
-				//return res.send(user);
 			} else {
 				return res.redirect('/admin?error=noPermissions');	
 			}
